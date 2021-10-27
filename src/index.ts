e0dc1e074ea267a1bc5068ae27bd2573412e4e65
Labnan <khalidmasum@iut-dhaka.edu>
2021-10-27 07:08:50 +0600

    Remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,43 @@
-import * as THREE from "three";
-import {GUI} from "dat.gui";
-import {MeshBasicMaterial, PlaneGeometry} from "three";
+import {
+    DirectionalLight, Mesh,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    Renderer, Scene,
+    SphereBufferGeometry,
+    WebGLRenderer
+} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 let canvas = document.querySelector("#c") as HTMLCanvasElement;
-const renderer = new THREE.WebGLRenderer({canvas});
-const gui = new GUI();
-const camera = new THREE.PerspectiveCamera( 40, 2, 0.1, 1000);
-const controls = new OrbitControls(camera,renderer.domElement);
+const renderer = new WebGLRenderer({canvas});
+const camera = new PerspectiveCamera(40, 2, 0.1, 1000);
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 5, 0);
 controls.update();
 
 
-camera.position.set(0,20,100);
+camera.position.set(0, 20, 100);
+camera.lookAt(0, 0, 0);
 
-const scene = new THREE.Scene();
+const scene = new Scene();
 
-const plane = new THREE.Mesh(new PlaneGeometry(100,100),new MeshBasicMaterial({color:0x449900}));
-scene.add(plane);
+const sphere = new Mesh(new SphereBufferGeometry(20, 1000), new MeshBasicMaterial({color: 0x449900}));
+scene.add(sphere);
 
 {
 
-    let light = new THREE.DirectionalLight("0xFFFFFF",1);
+    let light = new DirectionalLight("0xFFFFFF", 1);
     scene.add(light);
 }
 
 
-
-class AxisGridHelper{
-    private grid: THREE.GridHelper;
-    private axes: THREE.AxesHelper;
-    private _visible: boolean;
-
-    constructor(node: THREE.Object3D, units: number | undefined) {
-        const axes = new THREE.AxesHelper();
-        const axesMaterial = axes.material as THREE.Material;
-        axesMaterial.depthTest = false;
-        axes.renderOrder = 2;
-        node.add(axes);
-
-        const grid = new THREE.GridHelper(units,units);
-        const gridMaterial = grid.material as THREE.Material;
-        gridMaterial.depthTest = false;
-        grid.renderOrder = 1;
-        node.add(grid);
-
-        this.grid = grid;
-        this.axes = axes;
-        this._visible = this.grid.visible;
-    }
-
-    get visible() : boolean{
-        return this._visible;
-    }
-
-    set visible(v:boolean){
-      this._visible = v;
-      this.grid.visible = v;
-      this.axes.visible = v;
-    }
-
-
-}
-
-
-function resizeRendererToDisplaySize(renderer:THREE.Renderer){
+function resizeRendererToDisplaySize(renderer: Renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !==height;
-    if (needResize){
-        renderer.setSize(width,height,false);
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
     }
 
     return needResize;
